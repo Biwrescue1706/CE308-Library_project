093183793e2a3fcfbabdb7e12df38cd397a86d9e
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-04-18 11:21:27 +0700

    Update express.d.ts

--- Backend/src/types/express.d.ts
+++ Backend/src/types/express.d.ts
@@ -1,7 +1,7 @@
-import "express";
+import { Request } from "express";
 
-declare module "express" {
-  export interface Request {
+declare module "express-serve-static-core" {
+  interface Request {
     user?: {
       id: string;
       username: string;
